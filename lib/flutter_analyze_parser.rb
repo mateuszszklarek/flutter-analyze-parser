fa16bb79e43e97d3c32d0a4b9e16e6ac279727c2
Mateusz Szklarek <mateusz.szklarek@gmail.com>
2019-03-12 09:45:01 +0100

    Make parser bulletproof

--- lib/flutter_analyze_parser.rb
+++ lib/flutter_analyze_parser.rb
@@ -4,14 +4,16 @@ class FlutterAnalyzeParser
   def self.violations(input)
     filtered_input = filtered_input(input)
 
-    if filtered_input.first.include? "No issues found!"
+    if filtered_input.detect { |element| element.include? "No issues found!" }
       return []
     else
       violations = []
 
-      filtered_input.each do |line|
+      filtered_input
+        .select { |line| line.start_with? "info" }
+        .each do |line|
         _, description, file_with_line_number, rule = line.split(" • ")
-        file, line, = file_with_line_number.split(":")
+        file, line = file_with_line_number.split(":")
 
         violations << FlutterViolation.new(rule, description, file, line.to_i)
       end
@@ -24,6 +26,5 @@ class FlutterAnalyzeParser
     input.each_line
       .reject { |line| line.strip.empty? }
       .map(&:strip)
-      .drop(1)
   end
 end
